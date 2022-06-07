944bbc28494c5b604f64ee8b5a2f486fcd47969a
sdivakarla <sarah.divakarla@gmail.com>
2022-06-07 18:52:27 -0400

    Challenge

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -10,5 +10,5 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 )
 #Summary Table of Suspension Coil Data
 total_summary <- coil_table %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),StdDev=sd(PSI),Var_PSI=var(PSI), .groups = 'keep') #create summary table with multiple columns
-
 lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),StdDev=sd(PSI),Var_PSI=var(PSI), .groups = 'keep') #create summary table
+
